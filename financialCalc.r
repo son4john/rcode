7b0eb1439414763866c7da1265b95f8f0db63130
son4john <49545823+son4john@users.noreply.github.com>
2023-08-29 08:03:44 -0500

    Add files via upload
    inital commit

---
+++ financialCalc.r
@@ -0,0 +1,13 @@
+anniversaryBudget = 500 #August 23
+birthdayBudget = 500 #May 4
+fathersBudget = 500 #June 16
+christmasBudget = 500 #December 25
+
+monthlyBudget = 600
+yearlyBudget = 600 * 12
+
+personalBudgetTotal = sum(anniversaryBudget,birthdayBudget,fathersBudget,christmasBudget,yearlyBudget)
+dailyBudget = personalBudgetTotal/360
+
+print(personalBudgetTotal)
+print(dailyBudget)
