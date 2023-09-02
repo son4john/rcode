004f094632f66901ffe8e60995dd8877cfb00130
son4john <johnson.oonnoonni@gmail.com>
2023-09-01 22:05:34 -0500

    Updating Car calculator

---
+++ finCar.r
@@ -0,0 +1,19 @@
+budgetMax = 45000
+yrsUsed = 20
+
+yrdep = budgetMax/yrsUsed
+print("new car")
+print(yrdep)
+
+accord= 28000
+accordyrs = 20
+yrdep = accord/accordyrs
+print("accord")
+print(yrdep)
+
+pilot = 35000
+pilotyrs = 15
+print("pilot")
+yrdep = pilot/pilotyrs
+print(yrdep)
+
